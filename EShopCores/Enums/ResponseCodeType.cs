8991aaf4012eef87ef481867abf3449e1f89a869
LIN-CHIN <s235068267@gmail.com>
2023-09-19 10:55:12 +0800

    feat: add log helper

--- EShopCores/Enums/ResponseCodeType.cs
+++ EShopCores/Enums/ResponseCodeType.cs
@@ -13,6 +13,7 @@ namespace EShopCores.Enums
     /// </summary>
     public enum ResponseCodeType
     {
+        #region 系統訊息 (大方向) 0-1000
         /// <summary>
         /// 成功
         /// </summary>
@@ -20,6 +21,15 @@ namespace EShopCores.Enums
         [ResponseMessage("Success", Description = "成功")]
         Success = 0,
 
+        /// <summary>
+        /// 系統錯誤
+        /// </summary>
+        [Description("系統錯誤")]
+        [ResponseMessage("SystemError", Description = "系統錯誤")]
+        SystemError = 1,
+        #endregion
+
+        #region 參數問題 1000-1999
         /// <summary>
         /// Request參數錯誤
         /// </summary>
@@ -33,5 +43,25 @@ namespace EShopCores.Enums
         [Description("ModelBinding錯誤")]
         [ResponseMessage("ModelBindingError", Description = "ModelBinding錯誤")]
         ModelBindingError = 1001,
+        #endregion
+
+        #region 資料庫、資料問題  2000-3000
+
+        /// <summary>
+        /// DB內部錯誤
+        /// </summary>
+        [Description("DB內部錯誤")]
+        [ResponseMessage("DBInnerError", Description = "DB內部錯誤")]
+        DBInnerError = 2000,
+
+        /// <summary>
+        /// 違反Unique Key，資料重複
+        /// </summary>
+        [Description("違反Unique Key，資料重複")]
+        [ResponseMessage("UniqueDataDuplicate", Description = "違反Unique Key，資料重複")]
+        UniqueDataDuplicate = 2001
+
+
+        #endregion
     }
 }
---
+++ EShopCores/Models/LogModel.cs
@@ -0,0 +1,30 @@
+﻿using EShopCores.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopCores.Models
+{
+    /// <summary>
+    /// 紀錄Log用的Model
+    /// </summary>
+    public class LogModel
+    {
+        /// <summary>
+        /// 事件id
+        /// </summary>
+        public string EventId { get; set; }
+
+        /// <summary>
+        /// 訊息類型
+        /// </summary>
+        public LogMessageType LogMessageType { get; set; }
+
+        /// <summary>
+        /// Request/Response Body
+        /// </summary>
+        public object? Body { get; set; }
+    }
+}
