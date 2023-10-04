d370c4de88e9756b5d7607741786733357622988
LIN-CHIN <s235068267@gmail.com>
2023-10-04 15:25:50 +0800

    feat(shopRole): add shop role CURD and enable/disable api

---
+++ EShopAPI/Cores/ShopRoles/DTOs/InsertShopRoleDto.cs
@@ -0,0 +1,76 @@
+﻿using EShopAPI.Cores.ShopUsers;
+using EShopCores.Extensions;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EShopAPI.Cores.ShopRoles.DTOs
+{
+    /// <summary>
+    /// 新增角色用的DTO
+    /// </summary>
+    public class InsertShopRoleDto
+    {
+        /// <summary>
+        /// 角色代碼
+        /// </summary>
+        [JsonRequired]
+        [StringLength(50)]
+        public string Number { get; set; } = null!;
+
+        /// <summary>
+        /// 角色名稱
+        /// </summary>
+        [JsonRequired]
+        [StringLength(50)]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// 是否啟用
+        /// </summary>
+        /// <remarks>
+        /// 預設 = true
+        /// </remarks>
+        [JsonRequired]
+        public bool IsEnable { get; set; } = true;
+
+        /// <summary>
+        /// 建立者
+        /// </summary>
+        [JsonRequired]
+        public string CreateUser { get; set; } = null!;
+
+        /// <summary>
+        /// 建立時間
+        /// </summary>
+        [JsonIgnore]
+        public long CreateDate { get; set; } = DateTime.UtcNow.GetUnixTimeMillisecond();
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string? Remarks { get; set; }
+
+        /// <summary>
+        /// 多國語系
+        /// </summary>
+        public Dictionary<string, string>? Language { get; set; }
+
+        /// <summary>
+        /// 轉成實體
+        /// </summary>
+        /// <returns></returns>
+        public ShopRole ToEntity()
+        {
+            return new ShopRole
+            {
+                Number = Number,
+                Name = Name,
+                IsEnable = IsEnable,
+                CreateUser = CreateUser,
+                CreateDate = CreateDate,
+                Remarks = Remarks,
+                Language = System.Text.Json.JsonSerializer.SerializeToDocument(Language)
+            };
+        }
+    }
+}
--- EShopAPI/Cores/ShopRoles/Services/ShopRoleService.cs
+++ EShopAPI/Cores/ShopRoles/Services/ShopRoleService.cs
@@ -1,14 +1,65 @@
-﻿namespace EShopAPI.Cores.ShopRoles.Services
+﻿using EShopAPI.Cores.ShopRoles.DAOs;
+using EShopAPI.Cores.ShopRoles.DTOs;
+using EShopCores.Errors;
+using EShopCores.Responses;
+
+namespace EShopAPI.Cores.ShopRoles.Services
 {
     /// <summary>
     /// 商店角色 Service Interface
     /// </summary>
     public class ShopRoleService : IShopRoleService
     {
-        ///<inheritdoc/>
-        public Task<ShopRole?> GetById(long id)
+        private readonly IShopRoleDao _shopRoleDao;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="shopRoleDao"></param>
+        public ShopRoleService(IShopRoleDao shopRoleDao) 
         {
-            throw new NotImplementedException();
+            _shopRoleDao = shopRoleDao;
+        }
+
+        ///<inheritdoc/>
+        public IQueryable<ShopRole> Get(QueryShopRoleDto queryDto)
+        {
+            return _shopRoleDao.Get(queryDto);
+        }
+
+        ///<inheritdoc/>
+        public async Task<ShopRole?> GetByIdAsync(long id)
+        {
+            return await _shopRoleDao.GetByIdAsync(id);
+        }
+
+        ///<inheritdoc/>
+        public async Task<ShopRole> InsertAsync(InsertShopRoleDto insertDto)
+        {
+            return await _shopRoleDao.InsertAsync(insertDto.ToEntity());
+        }
+
+        ///<inheritdoc/>
+        public async Task UpdateAsync(UpdateShopRoleDto updateDto)
+        {
+            ShopRole shopRole = await _shopRoleDao.ThrowNotFindByIdAsync(updateDto.Id);
+            await _shopRoleDao.UpdateAsync(updateDto.SetEntity(shopRole));
+        }
+
+        ///<inheritdoc/>
+        public async Task EnableAsync(long id, bool isEnable)
+        {
+            ShopRole shopRole = await _shopRoleDao.ThrowNotFindByIdAsync(id);
+            if (isEnable)
+            {
+                shopRole.IsEnable = true;
+                await _shopRoleDao.UpdateAsync(shopRole);
+            }
+            else 
+            {
+                shopRole.IsEnable = false;
+                await _shopRoleDao.UpdateAsync(shopRole);
+            }
         }
     }
 }
