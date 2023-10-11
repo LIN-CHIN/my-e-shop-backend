85cb4c7b69f158cd748e5e832092cce62a013793
LIN-CHIN <s235068267@gmail.com>
2023-10-11 11:27:47 +0800

    feat: add language setting class

---
+++ EShopCores/Enums/LanguageType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopCores.Enums
+{
+    /// <summary>
+    /// 語言類型
+    /// </summary>
+    public enum LanguageType
+    {
+        /// <summary>
+        /// 台灣(繁體中文)
+        /// </summary>
+        [Description("台灣(繁體中文)")]
+        TW = 1,
+
+        /// <summary>
+        /// 英文
+        /// </summary>
+        [Description("英文")]
+        EN_US = 2,
+    }
+}
---
+++ EShopCores/Json/LanguageJson.cs
@@ -0,0 +1,28 @@
+﻿using EShopCores.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EShopCores.Json
+{
+    /// <summary>
+    /// 設定多國語係Json的格式
+    /// </summary>
+    public class LanguageJson
+    {
+        /// <summary>
+        /// 語言key
+        /// </summary>
+        [JsonRequired]
+        public LanguageType LanguageKey { get; set; }
+
+        /// <summary>
+        /// 語言的值
+        /// </summary>
+        [JsonRequired]
+        public string LanguageValue { get; set; } = null!;
+    }
+}
