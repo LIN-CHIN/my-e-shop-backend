ed1067e58e7c134305b8b34f56d4d4ed8a7714d1
LIN-CHIN <s235068267@gmail.com>
2023-10-24 15:11:56 +0800

    feat: add customVariantAttribute table

--- EShopCores/Json/VariantAttributeJson.cs
+++ EShopCores/Json/VariantAttributeJson.cs
@@ -5,6 +5,19 @@ namespace EShopCores.Json
     /// <summary>
     /// 變種屬性Json格式
     /// </summary>
+    /// <remarks>
+    /// 範例:
+    /// [
+    ///  {
+    ///     "key": "custom_variant_attribute 的number",
+    ///     "value": "custom_variant_attribute 的options欄位中的id"
+    ///  },
+    ///  {
+    ///     "key": "color",
+    ///     "value": "123kopedk00123mweofmwpmo"  //GUID之類的唯一碼()
+    ///  }
+    ///]
+    /// </remarks>
     public class VariantAttributeJson
     {
         /// <summary>
@@ -17,8 +30,9 @@ namespace EShopCores.Json
 
         /// <summary>
         /// 變種屬性的值
-        /// ex: red
         /// </summary>
+        /// <remarks>
+        /// </remarks>
         [JsonRequired]
         [JsonPropertyName("value")]
         public string Value { get; set; } = null!;
